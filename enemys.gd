e0b89dd2863262cf27cd0ac01a946ed60eece7c2
rolandbeni <beniroland12@gmail.com>
2024-04-14 17:58:09 +0200

    correcting the enemy's code

--- enemys.gd
+++ enemys.gd
@@ -1,15 +1,38 @@
 extends CharacterBody2D
 
-var enemy_scene = preload("res://Sprite2D.gd") # Load the enemy scene
-var num_enemies = 5 # Number of enemies to spawn
+@onready var enemy = $Sprite2D 
+var timer = 0
+var appear_time = 3
+var invisible_duration = 1
+var invisible_time =1
+var is_visible = true
+
 
 func _ready():
-	spawn_enemies(num_enemies)
+	
+	enemy.position= Vector2(randf_range(150, get_viewport_rect().size.x-150),
+	randf_range(75, get_viewport_rect().size.y-75)) 
+	
+	timer = appear_time
+	pass
 
-func spawn_enemies(num):
-	for i in range(num):
-		var new_enemy = enemy_scene.instance() # Instantiate the enemy scene
-		var random_position = Vector2(randf_range(0, get_viewport_rect().size.x), randf_range(0, get_viewport_rect().size.y)) # Generate a random position on the screen
-		new_enemy.position = random_position # Set the enemy's position to the random position
-		get_parent().add_child(new_enemy) # Add the enemy to the parent of the character (assuming character is a child of another node)
+func _process(delta):
 
+	timer -= delta
+
+
+	if timer <= 0:
+		is_visible = !is_visible
+		if is_visible:
+			respawn_enemy()
+			timer = appear_time
+		else:
+			hide_enemy()
+			timer = invisible_time
+
+func respawn_enemy():
+	enemy.position= Vector2(randf_range(150, get_viewport_rect().size.x-150), randf_range(75, get_viewport_rect().size.y-75))
+	show() 
+
+func hide_enemy():
+	hide()
