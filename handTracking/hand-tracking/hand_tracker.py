c00f475683b072d53c10bb586fd08fa693d59ba2
rolandbeni <beniroland12@gmail.com>
2024-04-16 03:47:36 +0200

    resize the camera's image

--- handTracking/hand-tracking/hand_tracker.py
+++ handTracking/hand-tracking/hand_tracker.py
@@ -20,6 +20,9 @@ while True:
     xx, yy=0, 0
     success, img = cap.read()
     img = cv2.flip(img, 1)
+    ratio=600/img.shape[1]
+    dim=(int(img.shape[0]*ratio),600)
+    img=cv2.resize(img, dim, interpolation=cv2.INTER_LINEAR)
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     result = hands.process(imgRGB)
     lmList = []
@@ -52,7 +55,7 @@ while True:
             pyautogui.mouseDown(xx, yy, "right")
             time.sleep(0.1)
             pyautogui.mouseUp(xx, yy, "right")
-            
+
         if lmList[tipIds[2]][2] < lmList[tipIds[2] - 2][2]:
             pyautogui.mouseDown(xx,yy,"left")
             time.sleep(0.1)
